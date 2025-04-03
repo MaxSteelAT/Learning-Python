08fb25db06975bf4ecaeb2262f357be9ec8a8ae1
Max Steel <mtra.ingrid.lopez@gmail.com>
2025-04-03 13:29:10 -0700

    Introduction to python.

---
+++ 1.intro.py
@@ -0,0 +1,27 @@
+"""Intrudcción a Python"""
+# Imprime valores
+print("Hola mundo!")
+
+# Imprime el texto la cantidad de veces que se específica.
+print("El weta " * 4)
+
+"""
+
+NOTAS
+
+1. Usar tres veces las dobles comillas te ayuda a guaradar texto que necesitan más de una línea. 
+2. Instala pylint para visualizar los posibles errores de tu código.
+3. Siempre deja una línea extra al final de tu código para evitar errores.
+
+Comandos
+
+Comando para abrir la terminal 
+CTRL + `
+
+Comando para visualizar errores en la terminal 
+Shift + CTRL + M
+
+Comando para abrir los errores encontrados por pylint 
+ALT + F8
+
+"""
